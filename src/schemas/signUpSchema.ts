7605e53ced83748b84f531f2ae2bca7510616918
Himanshu Rai <himanshuraimau9@gmail.com>
2024-05-28 22:44:40 +0530

    chore: Update signUpSchema to signUpSchema in signUpSchema.ts

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -7,7 +7,7 @@ export const usernameValidation = z.string().min(2,"Username must be atleast 2 c
 
 
 
-export const signupSchema = z.object({
+export const signUpSchema = z.object({
     username: usernameValidation,
     email: z.string().email({message:"Invalid email address"}),
     password: z.string().min(6,"Password must be atleast 6 characters"),
